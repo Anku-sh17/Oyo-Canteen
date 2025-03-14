d3972670b9174424f3035286b14ca3f4da5aff96
Ankush-Adnabu <ankush@adnabu.com>
2025-03-14 17:17:31 +0530

    Revamp the admin page

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () =>{
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('DB connected');
+    })
+    .catch((err) => {
+        console.error('DB connection error:', err);
+    });
+}
